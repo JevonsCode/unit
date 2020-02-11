2bb0608f636febc73a3c3adb76331b88797d59ff
Jevons <tmoonlee@qq.com>
2020-02-11 15:00:17 +0800

    init react test

--- snapshot 功能/行内 snapshot 功能/demo.js
+++
@@ -1,8 +0,0 @@
-export const generateConfig = () => {
-    return {
-        server: "http://0.0.0.0",
-        port: 8083,
-        domain: "local",
-        time: new Date()
-    }
-}
--- 普通独立方法/math.test.js
+++
@@ -1,9 +0,0 @@
-import { add, minus } from "./math";
-
-test('测试加法 3 + 7', () => {
-    expect(add(3, 7)).toBe(10)
-})
-
-test('测试加法 3 - 7', () => {
-    expect(minus(3, 7)).toBe(-4)
-})
